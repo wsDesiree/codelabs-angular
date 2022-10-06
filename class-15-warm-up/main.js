93d2a203541b69aace1de1ef81c27f72b6403945
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-10-06 17:52:10 -0500

    update

--- class-15-warm-up/main.js
+++ class-15-warm-up/main.js
@@ -13,4 +13,10 @@ for(let i = 0; i <= 50; i++)(
 numbers = numbers.filter((num)=>{
     return num % 2 != 0;
 })
+let sum = 0;
+for(let i =0; i<numbers.length;i++){
+    sum+= numbers[i];
+
+}
 console.log(numbers)
+console.log(sum)
